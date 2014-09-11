d298065ae11989445074fa1cbad117fc07138f37
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-09-11 17:20:11 +0400

    String-based templating

--- 04-interpolation/interpolation.js
+++ 04-interpolation/interpolation.js
@@ -1,15 +1,29 @@
 var assert = require('assert');
 
-function render(template, context){
+function render(template){
   assert(typeof template == 'string');
 
-  return template.replace(/{{(\S+)}}/g, function (match, prop) {
-    return eval('context.' + prop);
-  });
+  var interpolatedTokens = [],
+      stringTokens = [],
+      match,
+      re = /{{([^}}]*)}}/g,
+      lastIndex = 0;
+  while (match = re.exec(template)) {
+    stringTokens.push(template.slice(lastIndex, match.index));
+    interpolatedTokens.push(match[1]);
+    lastIndex = re.lastIndex;
+  }
+  stringTokens.push(template.slice(lastIndex));
+  return function (context) {
+    return stringTokens.reduce(function (previous, current, index) {
+      return previous + current +
+             (index === stringTokens.length - 1 ? '' : eval('context.' + interpolatedTokens[index]));
+    }, '');
+  };
 }
 
-var tpl = '{{var1}} {{var2.someProp}}';
+var tpl = 'blah {{var1}} {{var2.someProp}} bbbb';
 
-console.log(render(tpl, {var1: 'something', var2: {someProp: 'something else'}}));
+console.log(render(tpl)({var1: 'something', var2: {someProp: 'something else'}}));
 
 module.exports = render;
