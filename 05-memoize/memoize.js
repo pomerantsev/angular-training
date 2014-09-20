f7f9083a91928cbfc53de2cce42e92f8b13a8fca
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-09-20 19:09:20 +0400

    Polish the implementation and write another test

--- 05-memoize/memoize.js
+++ 05-memoize/memoize.js
@@ -2,10 +2,11 @@ var assert = require('assert');
 
 var memoize = function (func) {
   var memoized = function () {
-    if (memoized.cache.hasOwnProperty(arguments[0])) {
-      return memoized.cache[arguments[0]];
+    var key = '' + arguments[0];
+    if (memoized.cache.hasOwnProperty(key)) {
+      return memoized.cache[key];
     } else {
-      var result = memoized.cache[arguments[0]] = func.apply(null, arguments);
+      var result = memoized.cache[key] = func.apply(this, arguments);
       return result;
     }
   };
@@ -39,6 +40,23 @@ var assertions = [
     // Third call with a different argument
     assert.equal(memoizedFunc(2), 4);
     assert.equal(counter, 2);
+  },
+  function () {
+    // It treats strings and integers as the same argument
+    var counter = 0;
+    var func = function (x) {
+      counter++;
+      return x + 1;
+    };
+    var memoizedFunc = memoize(func);
+
+    // First call
+    assert.equal(memoizedFunc(1), 2);
+
+    // Second call - wrong result
+    assert.equal(memoizedFunc('1'), 2);
+
+    assert.equal(counter, 1);
   }
 ];
 
